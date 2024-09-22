9ca3b265f761dc16650e991f534b98e9044d344a
Subikesh <subikeshps@gmail.com>
2024-09-22 11:20:20 +0530

    Updated repository for centralised news and ai access

---
+++ shared/src/commonMain/kotlin/com/spacey/newsbuddy/NewsRepository.kt
@@ -0,0 +1,60 @@
+package com.spacey.newsbuddy
+
+import com.spacey.newsbuddy.base.Preference
+import com.spacey.newsbuddy.genai.Conversation
+import com.spacey.newsbuddy.genai.GenerativeAiService
+import com.spacey.newsbuddy.news.NewsApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+class NewsRepository(private val newsApiService: NewsApiService, private val generativeAiService: GenerativeAiService) {
+
+    private var newsPreference: String by Preference(Preference.NEWS_RESPONSE)
+    private var aiResponse: String by Preference(Preference.AI_REPONSE)
+
+    suspend fun getNewsConversation(): Result<List<Conversation>> {
+        return if (aiResponse.isEmpty()) {
+            val news = getTodaysNews()
+            news.convert {
+                log("News", "News response: $it")
+                generativeAiService.runPrompt(it).map { aiMsg ->
+                    aiResponse = aiMsg
+                    parseJson(aiMsg)
+                }
+            }
+        } else {
+            Result.success(parseJson(aiResponse))
+        }
+    }
+
+    suspend fun getTodaysNews(): Result<String> = withContext(Dispatchers.IO) {
+        try {
+            if (newsPreference.isEmpty()) {
+                newsApiService.getTodaysTopHeadlines().let {
+                    if (it.getOrThrow().jsonObject.getValue("totalResults").jsonPrimitive.int == 0) {
+                        throw Exception("Empty articles list was returned from news API")
+                    }
+                    it.map { result ->
+                        newsPreference = result.toString()
+                    }
+                }
+            }
+            Result.success(newsPreference)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    private fun parseJson(json: String): List<Conversation> {
+        val jsonObject: JsonObject = Json.decodeFromString(json)
+        return Json.decodeFromJsonElement(jsonObject["news"]!!)
+    }
+
+}
