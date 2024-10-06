df68e9cbda14b444180d45705669d50e1c89978f
Subikesh <subikeshps@gmail.com>
2024-10-06 23:09:31 +0530

    Home screen with text and account button on top

--- androidApp/src/main/java/com/spacey/newsbuddy/home/HomeScreen.kt
+++ androidApp/src/main/java/com/spacey/newsbuddy/home/HomeScreen.kt
@@ -4,14 +4,16 @@ import android.speech.tts.TextToSpeech
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material3.Card
@@ -19,6 +21,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -63,82 +66,88 @@ fun HomeScreen(
         Log.d("Speak", it.toString())
     }
     val uriHandler = LocalUriHandler.current
-    Column {
-        when (val state = uiState) {
-            is ListedUiState.Loading -> {
-                CenteredColumn {
-                    CircularProgressIndicator()
-                    Text(state.message)
-                }
+    when (val state = uiState) {
+        is ListedUiState.Loading -> {
+            CenteredColumn {
+                CircularProgressIndicator()
+                Text(state.message)
             }
+        }
 
-            is ListedUiState.Success -> {
-                var currentSpeaking: Int by remember {
-                    mutableIntStateOf(-1)
+        is ListedUiState.Success -> {
+            var currentSpeaking: Int by remember {
+                mutableIntStateOf(-1)
+            }
+            setFabConfig(FabConfig {
+                if (textToSpeech.isSpeaking) {
+                    setFabIcon(Icons.Outlined.PlayArrow)
+                    textToSpeech.stop()
+                } else {
+                    textToSpeech.converse(state.conversations, 0)
                 }
-                setFabConfig(FabConfig {
-                    if (textToSpeech.isSpeaking) {
-                        setFabIcon(Icons.Outlined.PlayArrow)
-                        textToSpeech.stop()
-                    } else {
-                        textToSpeech.converse(state.conversations, 0)
-                    }
-                })
-                textToSpeech.setOnUtteranceProgressListener(NewsSpeechListener(setFabIcon) {
-                    currentSpeaking = it
-                })
-                LazyColumn(contentPadding = PaddingValues()) {
-                    item {
-                        Row(Modifier.padding(16.dp)) {
-                            Text(
-                                "Hello,\nLatest news!",
-                                Modifier.weight(1f).padding(bottom = 16.dp),
-                                // TODO: Text size based on screen
-                                style = MaterialTheme.typography.displayMedium,
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
-                            IconButton(
-                                onClick = {
-                                    viewModel.promptTodaysNews(true)
-                                }
-                            ) {
-                                Icon(Icons.Default.Refresh, "Refresh", tint = MaterialTheme.colorScheme.onSurface)
+            })
+            textToSpeech.setOnUtteranceProgressListener(NewsSpeechListener(setFabIcon) {
+                currentSpeaking = it
+            })
+            LazyColumn(contentPadding = PaddingValues()) {
+                item {
+                    Row(Modifier.padding(16.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                        IconButton(onClick = { /*TODO*/ }, colors = IconButtonDefaults.iconButtonColors(MaterialTheme.colorScheme.secondaryContainer)) {
+                            Icon(Icons.Default.AccountCircle, contentDescription = "Account", tint = MaterialTheme.colorScheme.onSurface)
+                        }
+                        IconButton(
+                            colors = IconButtonDefaults.iconButtonColors(MaterialTheme.colorScheme.secondaryContainer),
+                            onClick = {
+                                viewModel.promptTodaysNews(true)
                             }
+                        ) {
+                            Icon(Icons.Default.Refresh, "Refresh", tint = MaterialTheme.colorScheme.onSurface)
                         }
                     }
-                    itemsIndexed(items = state.conversations) { i, conversation ->
-                        val weight = if (i == currentSpeaking) FontWeight.ExtraBold else null
-                        val shape = if (i == 0) RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp) else RoundedCornerShape(0.dp)
-                        val textPadding = if (i == 0) PaddingValues(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp) else PaddingValues(vertical = 8.dp, horizontal = 16.dp)
-                        Card(shape = shape, colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer)) {
-                            Text(text = conversation.content,
-                                modifier = Modifier
-                                    .padding(textPadding)
-                                    .combinedClickable(onLongClickLabel = "Open Url", onLongClick = {
+                }
+                item {
+                    Text(
+                        "Good Morning!",
+                        Modifier.padding(16.dp),
+                        // TODO: Text size based on screen
+                        style = MaterialTheme.typography.displayMedium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+                itemsIndexed(items = state.conversations) { i, conversation ->
+                    val weight = if (i == currentSpeaking) FontWeight.ExtraBold else null
+                    val shape = if (i == 0) RoundedCornerShape(topStart = 35.dp, topEnd = 35.dp) else RoundedCornerShape(0.dp)
+                    val textPadding = if (i == 0) PaddingValues(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp) else PaddingValues(vertical = 8.dp, horizontal = 16.dp)
+                    Card(shape = shape, colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer)) {
+                        Text(text = conversation.content,
+                            modifier = Modifier
+                                .padding(textPadding)
+                                .combinedClickable(
+                                    onLongClickLabel = "Open Url",
+                                    onLongClick = {
                                         conversation.link?.let {
                                             uriHandler.openUri(it)
                                         }
                                     }) {
-                                        textToSpeech.converse(state.conversations, i)
-                                    },
-                                style = MaterialTheme.typography.bodyLarge.copy(
-                                    fontWeight = weight,
-                                    color = MaterialTheme.colorScheme.onSurface
-                                )
+                                    textToSpeech.converse(state.conversations, i)
+                                },
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = weight,
+                                color = MaterialTheme.colorScheme.onSurface
                             )
-                        }
+                        )
                     }
                 }
             }
+        }
 
-            is ListedUiState.Error -> {
-                CenteredColumn {
-                    Text(
-                        state.message,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = MaterialTheme.colorScheme.error
-                    )
-                }
+        is ListedUiState.Error -> {
+            CenteredColumn {
+                Text(
+                    state.message,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.error
+                )
             }
         }
     }
