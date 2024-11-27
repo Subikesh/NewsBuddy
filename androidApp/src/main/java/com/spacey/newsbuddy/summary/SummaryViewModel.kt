592409527ef849fd44f77e43af77889509014a1a
Subikesh <subikeshps@gmail.com>
2024-11-27 08:54:07 +0530

    Summary screen based on date

--- androidApp/src/main/java/com/spacey/newsbuddy/summary/SummaryViewModel.kt
+++ androidApp/src/main/java/com/spacey/newsbuddy/summary/SummaryViewModel.kt
@@ -9,8 +9,6 @@ import com.spacey.newsbuddy.serviceLocator
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 class SummaryViewModel : ViewModel() {
 
@@ -18,12 +16,11 @@ class SummaryViewModel : ViewModel() {
     val uiState: StateFlow<ListedUiState<SummaryParagraph>> = _uiState
 
     private val genAiRepository = serviceLocator.genAiRepository
-    private val cacheDate = LocalDate.now().minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE)
 
-    fun promptTodaysNews(forceRefresh: Boolean = false) {
+    fun promptNews(date: String, forceRefresh: Boolean = false) {
         _uiState.value = ListedUiState.Loading()
         viewModelScope.launch {
-            val newsConvo = genAiRepository.getNewsSummary(cacheDate, forceRefresh)
+            val newsConvo = genAiRepository.getNewsSummary(date, forceRefresh)
             _uiState.value = newsConvo.fold(onSuccess = {
                 ListedUiState.Success(it)
             }, onFailure = {
