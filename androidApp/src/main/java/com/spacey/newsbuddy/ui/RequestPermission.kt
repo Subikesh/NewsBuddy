808208f8e555eac7230ce150c67931f8e4888c9e
subikesh.ps <subikesh.ps@zohocorp.com>
2024-11-30 12:50:39 +0530

    Modification in notification request and denial lambdas

--- androidApp/src/main/java/com/spacey/newsbuddy/ui/RequestPermission.kt
+++ androidApp/src/main/java/com/spacey/newsbuddy/ui/RequestPermission.kt
@@ -1,8 +1,13 @@
 package com.spacey.newsbuddy.ui
 
-import android.Manifest
 import android.Manifest.permission.POST_NOTIFICATIONS
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,30 +25,79 @@ import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
 import com.spacey.newsbuddy.MainActivity
+import com.spacey.newsbuddy.settings.PermissionState
+import com.spacey.newsbuddy.settings.SettingsAccessor
+
 
 @Composable
-fun RequestNotificationPermission(onPermissionGranted: () -> Unit, onPermissionDenied: () -> Unit, onPermanentlyDeclined: () -> Unit) {
+fun RequestNotificationPermission(onPermissionGranted: () -> Unit, onPermissionDeclined: () -> Unit, onPermanentlyDenied: () -> Unit) {
     val context = LocalContext.current
-    if (context.isNotificationAllowed()) return
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU || context.isNotificationAllowed()) {
+        onPermissionGranted()
+        return
+    }
+    val isFirstTime = SettingsAccessor.notificationCount == 0
+    var isAlertPositive by remember { mutableStateOf(PermissionState.DISABLED) }
 
-    var isAlertPositive by remember { mutableStateOf(false) }
-    if (!isAlertPositive) {
-        AlertDialog(
-            title = { Text("Permission Request") },
-            text = { Text("We sync daily news and keep things ready before you open the app. Kindly allow notification permission in the upcoming alert. You can always modify this in the settings.") },
-            onDismissRequest = onPermissionDenied,
-            confirmButton = {
-                Button(onClick = {
-                    isAlertPositive = true
-                }) { Text("Sure") }
-            }, dismissButton = {
-                // TODO: dismiss and don't show notification permission
-            }, shape = RoundedCornerShape(20.dp)
-        )
+    if (isFirstTime || shouldShowRequestPermissionRationale(context as Activity, POST_NOTIFICATIONS)) { // TODO: returns true
+        when (isAlertPositive) {
+            PermissionState.DISABLED -> {
+                AlertDialog(
+                    title = { Text("Permission Request 🙏🏻") },
+                    text = { Text("We sync daily news and keep things ready before you open the app. Kindly allow notification permission in the upcoming alert. You can always modify this in the settings.") },
+                    onDismissRequest = onPermissionDeclined,
+                    confirmButton = {
+                        Button(onClick = {
+                            isAlertPositive = PermissionState.ENABLED
+                        }) { Text("Sure") }
+                    }, dismissButton = {
+                        if (isFirstTime) {
+                            Button(onClick = {
+                                onPermissionDeclined()
+                                isAlertPositive = PermissionState.DENIED
+                            }) { Text(text = "Maybe later") }
+                        }
+                    }, shape = RoundedCornerShape(20.dp),
+                )
+            }
+            PermissionState.ENABLED -> {
+                SettingsAccessor.notificationCount += 1
+                RequestPermission(POST_NOTIFICATIONS, onPermissionGranted, onPermissionDeclined, onPermanentlyDenied)
+            }
+            PermissionState.DENIED -> {
+                onPermissionDeclined()
+            }
+        }
+    } else {
+        onPermanentlyDenied()
     }
-    if (isAlertPositive) {
-        RequestPermission(POST_NOTIFICATIONS, onPermissionGranted, onPermissionDenied, onPermanentlyDeclined)
+}
+
+@Composable
+fun ShowNotificationDeniedAlert(action: String? = null, onPermissionGranted: () -> Unit, onDismiss: () -> Unit) {
+    val context = LocalContext.current
+    val settingsActivity = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
+        if (context.isNotificationAllowed()) {
+            Toast.makeText(context, "We'll keep you updated when the sync happens 😃", Toast.LENGTH_SHORT).show()
+            onPermissionGranted()
+        }
     }
+    AlertDialog(title = { Text(text = "We Can't Notify you 😞") }, text = {
+        Text(text = "The notification permission is denied. Kindly open the app settings and enable notification permission${if (action != null) " to enable $action" else ""}.")
+    }, onDismissRequest = onDismiss, dismissButton = {
+        Button(onClick = onDismiss) {
+            Text(text = "Later")
+        }
+    }, confirmButton = {
+        Button(onClick = {
+            settingsActivity.launch(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                val uri = Uri.fromParts("package", context.packageName, null)
+                setData(uri)
+            })
+        }) {
+            Text(text = "Get me there!")
+        }
+    }, shape = RoundedCornerShape(20.dp))
 }
 
 @Composable
@@ -53,20 +107,21 @@ fun RequestPermission(
     onPermissionDenied: () -> Unit,
     onPermanentlyDeclined: () -> Unit
 ) {
-
     val context = LocalContext.current
     if (ContextCompat.checkSelfPermission(LocalContext.current, permission) != PackageManager.PERMISSION_GRANTED) {
         val permissionLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted)
                 onPermissionGranted()
-            else if (shouldShowRequestPermissionRationale(context as MainActivity, POST_NOTIFICATIONS))
-                onPermissionDenied()
-            else
+            else if (!shouldShowRequestPermissionRationale(context as MainActivity, permission))
                 onPermanentlyDeclined()
+            else
+                onPermissionDenied()
         }
 
         SideEffect {
             permissionLauncher.launch(permission)
         }
+    } else {
+        onPermissionGranted()
     }
 }
