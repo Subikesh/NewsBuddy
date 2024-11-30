103895d4c5c463bd3ff873111e1bad07d1058fc2
subikesh.ps <subikesh.ps@zohocorp.com>
2024-11-30 12:51:18 +0530

    Settings VM

---
+++ androidApp/src/main/java/com/spacey/newsbuddy/settings/SettingsViewModel.kt
@@ -0,0 +1,34 @@
+package com.spacey.newsbuddy.settings
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.spacey.newsbuddy.workers.cancelDataSync
+import com.spacey.newsbuddy.workers.scheduleDataSync
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class SettingsViewModel : ViewModel() {
+    private val _syncState = MutableStateFlow(SettingsUiState(SettingsAccessor.dataSyncEnabled))
+    val syncState: StateFlow<SettingsUiState> = _syncState
+
+    fun enableDataSync(context: Context) {
+        SettingsAccessor.dataSyncEnabled = PermissionState.ENABLED
+        _syncState.value = syncState.value.copy(syncState = PermissionState.ENABLED)
+        scheduleDataSync(context)
+    }
+
+    fun disableSync(context: Context) {
+        SettingsAccessor.dataSyncEnabled = PermissionState.DISABLED
+        _syncState.value = syncState.value.copy(syncState = PermissionState.DISABLED)
+        cancelDataSync(context)
+    }
+
+    fun denyPermission() {
+        _syncState.value = syncState.value.copy(syncState = PermissionState.DENIED)
+        SettingsAccessor.dataSyncEnabled = PermissionState.DENIED
+    }
+}
+
+data class SettingsUiState(
+    val syncState: PermissionState
+)
