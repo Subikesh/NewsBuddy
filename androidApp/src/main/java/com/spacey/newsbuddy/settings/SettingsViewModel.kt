64dad194a9c6c40cec9667e484ac4e490c089966
subikesh.ps <subikesh.ps@zohocorp.com>
2024-12-07 21:16:03 +0530

    Data sync time support in viewmodel and state

--- androidApp/src/main/java/com/spacey/newsbuddy/settings/SettingsViewModel.kt
+++ androidApp/src/main/java/com/spacey/newsbuddy/settings/SettingsViewModel.kt
@@ -6,9 +6,17 @@ import com.spacey.newsbuddy.workers.cancelDataSync
 import com.spacey.newsbuddy.workers.scheduleDataSync
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import java.util.Calendar
 
 class SettingsViewModel : ViewModel() {
-    private val _syncState = MutableStateFlow(SettingsUiState(SettingsAccessor.dataSyncEnabled, SettingsAccessor.summaryFeatureEnabled))
+    private val _syncState = MutableStateFlow(
+        SettingsUiState(
+            SettingsAccessor.summaryFeatureEnabled,
+            SettingsAccessor.dataSyncEnabled,
+            SettingsAccessor.dataSyncTimePair.first,
+            SettingsAccessor.dataSyncTimePair.second
+        )
+    )
     val syncState: StateFlow<SettingsUiState> = _syncState
 
     fun enableSummary() {
@@ -22,9 +30,15 @@ class SettingsViewModel : ViewModel() {
     }
 
     fun enableDataSync(context: Context) {
+        val previousTime = SettingsAccessor.dataSyncTimePair
+        enableDataSync(context, previousTime.first, previousTime.second)
+    }
+
+    fun enableDataSync(context: Context, hour: Int, minute: Int) {
         SettingsAccessor.dataSyncEnabled = PermissionState.ENABLED
-        scheduleDataSync(context)
-        _syncState.value = syncState.value.copy(syncState = PermissionState.ENABLED)
+        SettingsAccessor.dataSyncTime = "$hour:$minute"
+        scheduleDataSync(context, getNextTime(hour, minute))
+        _syncState.value = syncState.value.copy(syncState = PermissionState.ENABLED, syncHour = hour, syncMinute = minute)
     }
 
     fun disableSync(context: Context) {
@@ -37,9 +51,23 @@ class SettingsViewModel : ViewModel() {
         _syncState.value = syncState.value.copy(syncState = PermissionState.DENIED)
         SettingsAccessor.dataSyncEnabled = PermissionState.DENIED
     }
+
+    private fun getNextTime(hour: Int, minute: Int): Calendar {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hour)
+            set(Calendar.MINUTE, minute)
+            set(Calendar.SECOND, 0)
+            if (before(Calendar.getInstance())) {
+                add(Calendar.DATE, 1)
+            }
+        }
+        return calendar
+    }
 }
 
 data class SettingsUiState(
+    val summaryFeatureEnabled: Boolean,
     val syncState: PermissionState,
-    val summaryFeatureEnabled: Boolean
+    val syncHour: Int,
+    val syncMinute: Int
 )
