59f35100c79c0c2bb74593154a208c5cc70cdd52
subikesh.ps <subikesh.ps@zohocorp.com>
2024-12-07 21:15:50 +0530

    Data sync time support in preference

--- shared/src/commonMain/kotlin/com/spacey/newsbuddy/settings/SettingsAccessor.kt
+++ shared/src/commonMain/kotlin/com/spacey/newsbuddy/settings/SettingsAccessor.kt
@@ -8,9 +8,15 @@ object SettingsAccessor {
         get() = PermissionState.valueOf(dataSyncEnabledPref)
         set(value) { dataSyncEnabledPref = value.name }
 
+    /** Data sync time string in format "HH:mm" */
+    var dataSyncTime: String by Preference("data_sync_time", "5:00")
+
     var notificationCount: Int by Preference("notification_count")
 
     var summaryFeatureEnabled: Boolean by Preference("summary_feature_enabled")
+
+    val dataSyncTimePair: Pair<Int, Int>
+        get() = dataSyncTime.split(':').map { it.toInt() }.let { it[0] to it[1] }
 }
 
 enum class PermissionState {
