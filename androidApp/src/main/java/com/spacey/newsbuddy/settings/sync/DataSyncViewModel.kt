c9286bf51ab7b826d995e067c39a66b69a95908a
Subikesh <subikeshps@gmail.com>
2024-12-08 23:51:24 +0530

    Data Sync view model method added

--- androidApp/src/main/java/com/spacey/newsbuddy/settings/sync/DataSyncViewModel.kt
+++ androidApp/src/main/java/com/spacey/newsbuddy/settings/sync/DataSyncViewModel.kt
@@ -1,13 +1,21 @@
 package com.spacey.newsbuddy.settings.sync
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
+import androidx.work.await
 import com.spacey.newsbuddy.ListedUiState
 import com.spacey.newsbuddy.datasync.SyncEntry
 import com.spacey.newsbuddy.serviceLocator
+import com.spacey.newsbuddy.workers.NewsSyncWorker
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DataSyncViewModel : ViewModel() {
 
@@ -20,6 +28,19 @@ class DataSyncViewModel : ViewModel() {
         viewModelScope.launch {
             val result = syncRepository.getSyncData()
             _uiState.value = ListedUiState.Success(result)
+        }
+    }
+
+    fun syncNow(context: Context) {
+        _uiState.value = ListedUiState.Loading()
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                WorkManager.getInstance(context).enqueue(OneTimeWorkRequestBuilder<NewsSyncWorker>().run {
+                    setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                    build()
+                }).await()
+            }
+            fetchLatestDataSyncs()
         }
     }
 }
