fd08035e528514bd471dccc68c0e6e43b7cf5e0f
subikesh.ps <subikeshps@gmail.com>
2024-12-26 22:57:58 +0530

    Test commit

--- shared/src/commonMain/kotlin/com/spacey/newsbuddy/common/DomainConstants.kt
+++ shared/src/commonMain/kotlin/com/spacey/newsbuddy/common/DomainConstants.kt
@@ -1,5 +1,5 @@
 package com.spacey.newsbuddy.common
 
-const val BASE_URL = "https://newsapi.org/v2/"
+const  val BASE_URL = "https://newsapi.org/v2/"
 const val NEWS_PAGE_SIZE = 50
 const val NEWS_LANGUAGE = "en"
